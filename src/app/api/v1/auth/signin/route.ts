54ac961edba371fb703ec293e8f10ba7ac7a3c0b
ghOst-vedant <gaikwadvedant30@gmail.com>
2025-06-05 15:56:05 +0530

    [auth]: basic auth for signin and signup

---
+++ src/app/api/v1/auth/signin/route.ts
@@ -0,0 +1,50 @@
+import { signInSchema, signUpSchema } from "@/lib/zod"
+import { NextRequest, NextResponse } from "next/server"
+import bcrypt from "bcrypt"
+import { db } from "@/lib/prisma"
+
+export async function POST(req: NextRequest) {
+    try {
+        const formData = await req.formData()
+        const email = formData.get("email")
+        const password = formData.get("password")
+        const data = { email, password }
+        const parsed = signInSchema.safeParse(data)
+        if (!parsed.success) {
+            return NextResponse.json({ errors: parsed.error }, { status: 400 })
+        }
+        const user = await db.user.findUnique({
+            where: { email: parsed.data.email },
+        })
+        if (!user) {
+            return NextResponse.json(
+                { message: "User not found." },
+                { status: 404 }
+            )
+        }
+        const isPasswordValid = await bcrypt.compare(
+            parsed.data.password,
+            user.password
+        )
+        if (!isPasswordValid) {
+            return NextResponse.json(
+                { message: "Invalid password." },
+                { status: 401 }
+            )
+        }
+
+        return NextResponse.json(
+            {
+                message: "Sign-in successful.",
+                user: { id: user.id, email: user.email },
+            },
+            { status: 200 }
+        )
+    } catch (error) {
+        console.log("Sign-in error", error)
+        return NextResponse.json(
+            { message: "An error occurred during signup." },
+            { status: 500 }
+        )
+    }
+}
---
+++ src/app/api/v1/auth/signup/route.ts
@@ -0,0 +1,46 @@
+import { signUpSchema } from "@/lib/zod"
+import { NextRequest, NextResponse } from "next/server"
+import bcrypt from "bcrypt"
+import { db } from "@/lib/prisma"
+export async function POST(req: NextRequest) {
+    try {
+        const formdata = await req.formData()
+        const username = formdata.get("username")
+        const email = formdata.get("email")
+        const password = formdata.get("password")
+        const data = { username, email, password }
+        const parsed = signUpSchema.safeParse(data)
+        if (!parsed.success) {
+            return NextResponse.json({ errors: parsed.error }, { status: 400 })
+        }
+        const signInUser = await db.user.findUnique({
+            where: { email: parsed.data.email },
+        })
+
+        if (signInUser) {
+            return NextResponse.json(
+                { message: "User already exists." },
+                { status: 409 }
+            )
+        }
+        const hashedPassword = await bcrypt.hash(parsed.data.password, 10)
+
+        const newuser = await db.user.create({
+            data: {
+                username: parsed.data.username,
+                email: parsed.data.email,
+                password: hashedPassword,
+            },
+        })
+        return NextResponse.json(
+            { message: "User created successfully.", user: newuser },
+            { status: 201 }
+        )
+    } catch (error) {
+        console.log("Signup error", error)
+        return NextResponse.json(
+            { message: "An error occurred during signup." },
+            { status: 500 }
+        )
+    }
+}
