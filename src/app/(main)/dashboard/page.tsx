8ac8039c23d30b0b68f551f9439015b9a02cee89
ghOst-vedant <gaikwadvedant30@gmail.com>
2025-06-09 11:57:35 +0530

    [update]: Started work on workspace

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-    return <div>Dashboard Page</div>
+import { getSession } from "@/lib/session"
+import { getLoggedInWokspace } from "./DashboardActions"
+import { redirect } from "next/navigation"
+import DashboardSetup from "@/components/dashboard-setup/dashboard-setup"
+
+export default async function DashboardPage() {
+    const session = await getSession()
+    if (!session) return
+
+    const workspace = await getLoggedInWokspace(session.userId as string)
+
+    if (!workspace)
+        return (
+            <div className="bg-background w-full m-auto flex justify-center items-center">
+                <DashboardSetup></DashboardSetup>
+            </div>
+        )
+    redirect(`/workspace/${workspace.id}`)
 }
